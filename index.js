08f5420fd32a2808c4c7136d761b8f8893eb54c3
Bismark Arroyo <bis.arroyo@gmail.com>
2024-09-10 14:42:20 -0600

    Add CORS support and user authentication routes to the Express app in index.js

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import express from 'express'
+import cors from 'cors'
 
 import 'dotenv/config'
 
-const app = express()
+import usersRoutes from './routes/auth.js'
 
+const app = express()
 const PORT = process.env.PORT || 3000
+
+// handle cors
+app.use(cors())
+
+// Lectura y parseo del body
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/api/v1/auth', usersRoutes)
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
