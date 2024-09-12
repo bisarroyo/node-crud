2afd7c56fa925acbed08eafd7e02dbdb510e2db2
Bismark Arroyo <bis.arroyo@gmail.com>
2024-09-11 21:21:09 -0600

    Refactor user model methods to return responses; ensures consistent async handling for user data operations

--- models/user.js
+++ models/user.js
@@ -1,37 +1,43 @@
 import { turso } from '../config/database.js'
 
 export const findUserByEmail = async (email) => {
-  await turso.execute({
+  const response = await turso.execute({
     sql: 'SELECT * FROM users WHERE email = ?',
     args: [email]
   })
+  return response
 }
 export const insertUser = async (email, name, last_name, password) => {
-  await turso.execute({
+  const response = await turso.execute({
     sql: 'INSERT INTO users (email, name, last_name, password) VALUES (?,?,?,?)',
     args: [email, name, last_name, password]
   })
+  return response
 }
 export const getUser = async (email) => {
-  await turso.execute({
+  const response = await turso.execute({
     sql: 'SELECT * FROM users WHERE email = ? ',
     args: [email]
   })
+  return response
 }
 export const getAllUsers = async () => {
-  await turso.execute({
+  const response = await turso.execute({
     sql: 'SELECT * FROM users'
   })
+  return response
 }
 export const deleteUser = async (email) => {
-  await turso.execute({
+  const response = await turso.execute({
     sql: 'DELETE FROM users WHERE email = ?',
     args: [email]
   })
+  return response
 }
 export const updateUser = async (email, name, last_name, password) => {
-  await turso.execute({
+  const response = await turso.execute({
     sql: 'UPDATE users SET name = ?, last_name = ?, password = ? WHERE email = ?',
     args: [name, last_name, password, email]
   })
+  return response
 }
