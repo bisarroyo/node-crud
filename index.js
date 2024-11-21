a9f64333b25580c7f6e9bf84c0f4cfb83a0d6f62
Bismark Arroyo <bis.arroyo@gmail.com>
2024-11-21 14:16:21 -0600

    add route for clients

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import 'dotenv/config'
 
 import usersRoutes from './routes/auth.js'
 import mailRoutes from './routes/mail.js'
+import clientRoutes from './routes/clients.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -21,6 +22,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1/auth', usersRoutes)
 app.use('/api/v1/mail', mailRoutes)
+app.use('/api/v1/clients', clientRoutes)
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
