494bb46f0b7b7470207d1d214cc5c7e98360ef70
Bismark Arroyo <bis.arroyo@gmail.com>
2024-11-21 14:16:39 -0600

    fix import name for fetch clients

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,9 +1,29 @@
 import { createMail } from '../helpers/mail.js'
+import { findClientsByCategory } from '../models/clients.js'
 
 export const sendMail = async (req, res) => {
   const { email, name } = req.body
   try {
     await createMail(email, name)
+    res.status(200).json({
+      ok: true
+    })
+  } catch (err) {
+    res.status(401).json({
+      ok: false,
+      msg: err.message
+    })
+  }
+}
+export const sendMailToAll = async (req, res) => {
+  const { category } = req.body
+  try {
+    const clients = await findClientsByCategory(category)
+    Promise.all(
+      clients.rows.map(async (client) => {
+        await createMail(client.email, client.name)
+      })
+    )
     res.status(200).json({
       ok: true
     })
