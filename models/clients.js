cc5145814f5a942e91450cc0205e67e4ebc7f0d2
Bismark Arroyo <bis.arroyo@gmail.com>
2024-11-21 14:17:14 -0600

    create model for clients with sql queries

--- models/clients.js
+++ models/clients.js
@@ -1,46 +1,52 @@
 import { turso } from '../config/database.js'
 
-export const findClientByEmail = async (email) => {
-  const response = await turso.execute({
-    sql: 'SELECT * FROM clients WHERE email = ?',
-    args: [email]
-  })
-  return response
-}
-export const findClientByname = async (name) => {
-  const response = await turso.execute({
-    sql: 'SELECT * FROM clients WHERE name = ?',
-    args: [name]
-  })
-  return response
-}
-
-export const insertClient = async (email, name, last_name, password) => {
-  const response = await turso.execute({
-    sql: 'INSERT INTO clients (email, name, company, ) VALUES (?,?,?,?)',
-    args: [email, name, last_name, password]
-  })
-  return response
-}
-export const removeClient = async (email) => {
-  const response = await turso.execute({
-    sql: 'DELETE FROM clients WHERE email = ?',
-    args: [email]
-  })
-  return response
-}
-
-export const updateClient = async (email, name, last_name, password) => {
-  const response = await turso.execute({
-    sql: 'UPDATE clients SET name = ?, last_name = ?, password = ? WHERE email = ?',
-    args: [name, last_name, password, email]
-  })
-  return response
-}
-export const getClientsByCategory = async (category) => {
-  const response = await turso.execute({
+export const findClientsByCategory = async (category) => {
+  const { rows } = await turso.execute({
     sql: 'SELECT * FROM clients WHERE category = ? ',
     args: [category]
   })
-  return response
+  return rows
+}
+export const findClientByEmail = async (email) => {
+  const { rows } = await turso.execute({
+    sql: 'SELECT * FROM clients WHERE email = ?',
+    args: [email]
+  })
+  return rows
+}
+export const findClientByName = async (name) => {
+  const { rows } = await turso.execute({
+    sql: 'SELECT * FROM clients WHERE name LIKE ?',
+    args: [`%${name}%`]
+  })
+  return rows
+}
+export const findClientById = async (id) => {
+  const { rows } = await turso.execute({
+    sql: 'SELECT * FROM clients WHERE id = ?',
+    args: [id]
+  })
+  return rows
+}
+
+export const insertClientDb = async (email, name, company, category) => {
+  const { rows } = await turso.execute({
+    sql: 'INSERT INTO clients (email, name, company, category) VALUES (?,?,?,?)',
+    args: [email, name, company, category]
+  })
+  return rows
+}
+export const updateClientDb = async (name, email, company, category) => {
+  const { rows } = await turso.execute({
+    sql: 'UPDATE clients SET name = ?, email = ?, company = ?, category = ? WHERE email = ?',
+    args: [name, email, company, category, email]
+  })
+  return rows
+}
+export const removeClientDb = async (email) => {
+  const { rows } = await turso.execute({
+    sql: 'DELETE FROM clients WHERE email = ?',
+    args: [email]
+  })
+  return rows
 }
